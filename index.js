ce15cae3c3815138efc93d500a6b009012ff263a
Paras Bagri <parasba85@gmail.com>
2024-11-21 16:03:10 +0530

    created Hybrid API, server side rendering and client side rendering both

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express");
-
+const users = require("./MOCK_DATA.json"); // taking users json data from my local directory
 const app = express(); // crated insteans
+const PORT = 1999; // is port me server run hoga
 
-app.get();
+console.log(users);
+// routes yaha per defined honge, ye bana hai moblie developer ke liye jo JSON Data send karta hai
+app.get("/api/users", (req, res) => {
+  // /api-> trow karega tab JSON DATA jayega mobile app ke liye or agar /users throw karega to HTML RENDERING jayegi Browsers ke liye
+  return res.json(users); // yaha pass mai client/ browser/ front end ko ek row data send kar rha hu or frontend reatJs ka use kar ke apne hisab se es row data ko render kar sakta hai
+});
+
+//ye hai HTML RENDERING jo browser me render hoga
+app.get("/users", (req, res) => {
+  // res.sendFile(__dirname + "/index.html"); // HTML file ko send kar rha hai
+  return res.send(`
+    <h1>Users List</h1>
+    <ol>
+      ${users.map((user) => `<li>${user.first_name}</li>`).join(" ")}
+    </ol>
+  `);
+}); // HTML file ko send kar rha hai
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
