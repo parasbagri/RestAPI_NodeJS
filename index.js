cb311869c355cf985a615938f58ec315a64998b6
Paras Bagri <parasba85@gmail.com>
2024-11-25 19:08:16 +0530

    added MVC, Authentication, Authorization

--- index.js
+++ index.js
@@ -5,13 +5,37 @@ const PORT = 1999; // is port me server run hoga
 const fs = require("fs");
 
 // console.log(users);
+
+// middleware function or we can say pluging
+app.use(express.urlencoded({ extended: true }));
+// second middlewere custom middleware
+app.use((req, res, next) => {
+  console.log("middleware custom1");
+  req.myName = "paras";
+  // return res.json({ message: "Hello from M1" });
+  next(); // agar es middleware ka kaam ho gya hai  next middleware ko run krega
+});
+
+app.use((req, res, next) => {
+  console.log("middleware custom2", req.myName);
+  // return res.json({ message: "Hello from M1" });
+  next(); // agar es middleware ka kaam ho gya hai  next middleware ko run krega
+  // res.end("Hello from M2");
+});
+
 // routes yaha per defined honge, ye bana hai moblie developer ke liye jo JSON Data send karta hai
 app.get("/api/users", (req, res) => {
   // /api-> trow karega tab JSON DATA jayega mobile app ke liye or agar /users throw karega to HTML RENDERING jayegi Browsers ke liye
+
+  // headers definitions
+  console.log("FrontEnd Side Headers:", req.headers);
+  // res.setHeader("Content-Type", "application/json"); // client/browser/front end se JSON Data send kar sakte hai
+  // res.setHeader("Access-Control-Allow-Origin", "*"); // client/browser/front end se access kar sakte hai
+  res.setHeader("Name", "Paras Bagri");
   return res.json(users); // yaha pass mai client/ browser/ front end ko ek row data send kar rha hu or frontend reatJs ka use kar ke apne hisab se es row data ko render kar sakta hai
 });
 
-//ye hai HTML RENDERING jo browser me render hoga
+//ye hai HTML RENDERING jo browser me render hoga es path per jab client URL ye wala dalega tab
 app.get("/users", (req, res) => {
   // res.sendFile(__dirname + "/index.html"); // HTML file ko send kar rha hai
   return res.send(`
@@ -66,9 +90,6 @@ app.get("/users", (req, res) => {
 // });
 //-----------------------------------------------------
 
-// middleware function or we can say pluging
-app.use(express.urlencoded({ extended: true }));
-
 // club or chaning the same routes
 app
   .route("/api/users/:id")
@@ -99,7 +120,7 @@ app.post("/api/users", (req, res) => {
   //   });
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "sucuceed", id: users.length });
+    return res.status(201).json({ status: "sucuceed", id: users.length });
   });
 }); // json data ko file me write kar rha hai
 
